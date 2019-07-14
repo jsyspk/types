7e41b4c36a03a54867a863893598fa5d4baa0b80
Ali Masood <ali@zenex.co.jp>
2019-07-14 10:30:46 +0900

    Adds path class

---
+++ lib/AnyPath.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace J\Types;
+
+use \InvalidArgumentException;
+abstract class AnyPath implements Path
+{
+    protected $path;
+
+    public function __construct(string $path)
+    {
+        if(empty($path))
+        {
+            throw new InvalidArgumentException("Given path '$path' is empty and can not be used as a valid path", 30000);
+        }
+        $this->path = $path;
+    }
+
+    public function value(): string
+    {
+        return $this->path;
+    }
+}
